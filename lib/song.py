6bc99261652234ae60891769686a9b66ac309616
Abdifatah <abdifatah6002@gmail.com>
2026-06-09 23:36:01 +0300

    Add Song class with class variables and properties

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,43 @@
+#!/usr/bin/env python3
+
+class classproperty:
+    """Descriptor to allow class-level properties."""
+    def __init__(self, func):
+        self.func = func
+    def __get__(self, obj, owner):
+        return self.func(owner)
+
 class Song:
-    pass
+    """Represents a song in a music library system."""
+
+    count = 0
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        """Initialize a Song with a name, artist, and genre."""
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1
+
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
+
+    @classproperty
+    def genres(cls):
+        """Return a list of all unique genres."""
+        return list(cls.genre_count.keys())
+
+    @classproperty
+    def artists(cls):
+        """Return a list of all unique artists."""
+        return list(cls.artist_count.keys())
